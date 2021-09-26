15afdcf6d7f645250044c6b9757557b0abe28aad
Johannes Juraske <broeserl1@gmail.com>
2021-09-26 16:36:30 +0200

    Edited project file & edited open.clj file

--- src/dactyl_pivot/dactyl-open.clj
+++ src/dactyl_pivot/dactyl-open.clj
@@ -26,7 +26,7 @@
 (def first-15u-row 0)                   ; controls which should be the first row to have 1.5u keys on the outer column
 (def last-15u-row 3)                    ; controls which should be the last row to have 1.5u keys on the outer column
 
-(def extra-row false)                   ; adds an extra bottom row to the outer columns
+(def extra-row true)                   ; adds an extra bottom row to the outer columns
 (def inner-column true)                 ; adds an extra inner column (two less rows than nrows)
 
 (def column-style :standard)
